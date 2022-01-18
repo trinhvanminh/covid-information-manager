5189f5bafedb011d488b950aa48bc7dd56f06058
soraly <lequangnam1617@gmail.com>
2022-01-18 19:58:24 +0700

    test if

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -20,8 +20,17 @@ class ListProductController {
     if (!req.authenticated) {
       res.redirect("/");
     } else {
-      res.render("./products/addProduct");
-      // Xử lý add product to database here!
+      // Xử lý edit product to database here!
+      // Đưa data cần edit vào lại trang add product - để người dùng có thể sửa
+      //   const { id } = req.params; Lấy cái ID ở trên params/path
+      //   const product = await Product.findById(id); Lấy cái product ở trên database - này là code cũ mongoDB
+      // Thay code tìm bằng Postgres nha bạn
+      //   res.render("./products/addProduct", { product });
+    //   Data test cái UI sửa
+      const product = {
+        a: "hi",
+      };
+      res.render("./products/addProduct", { product });
     }
   }
 }
