8d1ad394d6cbb1ae5bedd4935447630d9d28338b
trinhvanminh <minh.a1.no.0@gmail.com>
2022-01-18 16:16:40 +0700

    them nguoi, nguoi lien quan

--- controllers/manager.controller.js
+++ controllers/manager.controller.js
@@ -3,7 +3,7 @@ const e = require("express");
 class ManagerController {
   // GET /
   relatedCovidView(req, res, next) {
-    console.log(req);
+    // console.log(req);
     if (!req.authenticated) {
       res.redirect("/");
     } else {
@@ -30,10 +30,10 @@ class ManagerController {
       } else {
         require("../db")
           .query(
-            'SELECT * FROM public."Nguoi" left JOIN public."NoiDieuTri" on "Nguoi".dieutri_id = "NoiDieuTri"."DieuTri_id"'
+            'SELECT * FROM public."Nguoi" left JOIN public."NoiDieuTri" on "Nguoi".dieutri_id = "NoiDieuTri"."DieuTri_id" ORDER BY "Nguoi_id" ASC'
           )
           .then((data) => {
-            console.log(data);
+            // console.log(data);
             if (data.rowCount == 0) {
               console.log("khong co du lieu");
               res.render("manager/related", {
@@ -87,23 +87,41 @@ class ManagerController {
               authenticated: req.authenticated,
             });
           } else {
-            res.render("manager/addCovidUser", {
-              authenticated: req.authenticated,
-              DSnoidieutri: data.rows,
-            });
+            require("../db")
+              .query('SELECT * FROM public."Nguoi" ORDER BY "Nguoi_id" ASC')
+              .then((nguoi) => {
+                res.render("manager/addCovidUser", {
+                  authenticated: req.authenticated,
+                  DSnoidieutri: data.rows,
+                  DSNguoi: nguoi.rows,
+                  maxNguoiId: nguoi.rows[nguoi.rows.length - 1].Nguoi_id,
+                });
+              });
           }
-        });
+        })
+        .catch((err) => console.log(err));
     }
   }
   postCovidUser(req, res, next) {
     if (!req.authenticated) {
       res.redirect("/");
     } else {
-      const { hoten, cccd, diachi, trangthai, dieutri_id } = req.body;
+      // console.log(req.body);
+      const {
+        hoten,
+        cccd,
+        namsinh,
+        diachi,
+        trangthai,
+        dieutri_id,
+        lichsu,
+        Nguoi_id,
+        maxNguoiId,
+      } = req.body;
       require("../db")
         .query(
-          'INSERT INTO "Nguoi" (hoten, cccd, diachi, trangthai, dieutri_id) VALUES ($1, $2, $3, $4, $5);',
-          [hoten, cccd, diachi, trangthai, dieutri_id]
+          'INSERT INTO "Nguoi" (hoten, cccd, namsinh, diachi, trangthai, dieutri_id, lichsu) VALUES ($1, $2, $3, $4, $5, $6, $7);',
+          [hoten, cccd, namsinh, diachi, trangthai, dieutri_id, lichsu]
         )
         .then((data) => {
           if (data.rowCount == 0) {
@@ -111,10 +129,17 @@ class ManagerController {
             res.render("manager/addCovidUser", {
               authenticated: req.authenticated,
             });
+          } else if (Nguoi_id) {
+            require("../db").query(
+              'INSERT INTO "NguoiLienQuan" (nguoi_id, nlq_id) VALUES ($1, $2);',
+              [maxNguoiId, Nguoi_id]
+            );
+            res.redirect("manager/related-covid/list");
           } else {
             res.redirect("manager/related-covid/list");
           }
-        });
+        })
+        .catch((err) => console.log(err));
     }
   }
 }
