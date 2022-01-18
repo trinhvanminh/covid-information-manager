36a09db83848fc2181d6e41809ecc073f09335f9
soraly <lequangnam1617@gmail.com>
2022-01-18 23:09:15 +0700

    how to select many product

--- controllers/packageProduct.controller.js
+++ controllers/packageProduct.controller.js
@@ -13,7 +13,34 @@ class PackageProductController {
 
   // POST Add Package Product /
   addPackageProduct(req, res) {
-    res.render("./productPackages/addPackageProduct");
+    //   Data Test 
+    const products = [
+      {
+        id: 1,
+        name: "Product 1",
+        price: 100,
+        quantity: 10,
+      },
+      {
+        id: 2,
+        name: "Product 2",
+        price: 200,
+        quantity: 20,
+      },
+      {
+        id: 3,
+        name: "Product 3",
+        price: 300,
+        quantity: 30,
+      },
+      {
+        id: 4,
+        name: "Product 4",
+        price: 400,
+        quantity: 40,
+      },
+    ];
+    res.render("./productPackages/addPackageProduct", { products });
   }
 }
 
