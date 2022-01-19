01fd1406475e1a9c0c65fa396748451d22acb95f
soraly <lequangnam1617@gmail.com>
2022-01-19 13:40:07 +0700

    edit package

--- controllers/packageProduct.controller.js
+++ controllers/packageProduct.controller.js
@@ -13,34 +13,22 @@ class PackageProductController {
 
   // POST Add Package Product /
   addPackageProduct(req, res) {
-    //   Data Test 
-    const products = [
-      {
-        id: 1,
-        name: "Product 1",
-        price: 100,
-        quantity: 10,
-      },
-      {
-        id: 2,
-        name: "Product 2",
-        price: 200,
-        quantity: 20,
-      },
-      {
-        id: 3,
-        name: "Product 3",
-        price: 300,
-        quantity: 30,
-      },
-      {
-        id: 4,
-        name: "Product 4",
-        price: 400,
-        quantity: 40,
-      },
-    ];
+    //   Data Test
     res.render("./productPackages/addPackageProduct", { products });
+  }
+
+  editPackageProduct(req, res) {
+
+    const dataTest = {
+      id: 1,
+      name: "Package 1",
+      listProduct: [],
+      limitQuantity: 2,
+      limitPerson: 3,
+      limitTime: 10,
+    }
+
+    res.render("./productPackages/editPackageProduct", {dataTest});
   }
 }
 
--- routes/packageProduct.route.js
+++ routes/packageProduct.route.js
@@ -5,6 +5,8 @@ const { verifyToken } = require("../jwt");
 router.get("/", verifyToken, PackageProductController.listPackageProduct);
 router.get("/view/:id", verifyToken, PackageProductController.viewPackageProduct);
 router.get("/add", verifyToken, PackageProductController.addPackageProduct);
+router.get("/edit/:id", verifyToken, PackageProductController.editPackageProduct);
+
 
 
 
