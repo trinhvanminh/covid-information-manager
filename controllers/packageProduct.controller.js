a875175a71078b5b3828e2451d843efb64dce7de
soraly <lequangnam1617@gmail.com>
2022-01-19 13:59:03 +0700

    controller route edit

--- controllers/packageProduct.controller.js
+++ controllers/packageProduct.controller.js
@@ -18,17 +18,23 @@ class PackageProductController {
   }
 
   editPackageProduct(req, res) {
+    try {
+      const dataTest = {
+        id: "23121",
+        packageName: "Package 12222",
+        listProduct: [],
+        limitQuantity: 2,
+        limitPerson: 3,
+        limitTime: 10,
+      };
 
-    const dataTest = {
-      id: 1,
-      name: "Package 1",
-      listProduct: [],
-      limitQuantity: 2,
-      limitPerson: 3,
-      limitTime: 10,
+      if (req.params.id === dataTest.id) {
+        res.render("./productPackages/editPackageProduct", { dataTest });
+      }
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json({ status: false, message: error.message });
     }
-
-    res.render("./productPackages/editPackageProduct", {dataTest});
   }
 }
 
