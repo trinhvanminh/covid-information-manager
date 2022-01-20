9423edfc2063a0b2436c4909f7c9c413751ec772
soraly <lequangnam1617@gmail.com>
2022-01-20 15:23:48 +0700

    fix system user

--- controllers/userSystem.controller.js
+++ controllers/userSystem.controller.js
@@ -1,32 +1,44 @@
 const e = require("express");
+const LocalStorage = require("node-localstorage").LocalStorage,
+  localStorage = new LocalStorage("./scratch");
 
 class UserSystemController {
   // GET Information User /
   viewInforUser(req, res) {
-    res.render("./user/informationUser");
+    const sodu = localStorage.getItem("sodu");
+    res.render("./user/informationUser", {
+      authenticated: req.authenticated,
+      sodu,
+    });
   }
 
   // GET Notify Payment User /
   notifyPaymentUser(req, res) {
-    res.render("./user/notifyPayment");
+    res.render("./user/notifyPayment",{
+      authenticated: req.authenticated,
+    });
   }
 
   // GET Cart User /
   cartUser(req, res) {
-    res.render("./user/cartUser");
+    res.render("./user/cartUser",{
+      authenticated: req.authenticated,
+    });
   }
 
   // GET Checkout User /
   checkoutUser(req, res) {
-    res.render("./user/checkoutPayment");
+    res.render("./user/checkoutPayment",{
+      authenticated: req.authenticated,
+    });
   }
 
   // GET Balance User /
   balanceUser(req, res) {
-    res.render("./user/accountBalance");
+    res.render("./user/accountBalance",{
+      authenticated: req.authenticated,
+    });
   }
-
- 
 }
 
 module.exports = new UserSystemController();
