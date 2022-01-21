412dc4bc30a115a488c014d0d297d06d70561feb
soraly <lequangnam1617@gmail.com>
2022-01-21 08:37:24 +0700

    update

--- controllers/userSystem.controller.js
+++ controllers/userSystem.controller.js
@@ -70,7 +70,7 @@ class UserSystemController {
     try {
       const role = localStorage.getItem("role");
       const idWallet = localStorage.getItem("idPayMent");
-      const amount = 120000;
+      const amount = 100000;
       const hanMucThanhToan = localStorage.getItem("hanMucThanhToan");
       if (amount < hanMucThanhToan) {
         res.render("./user/checkoutPayment", {
